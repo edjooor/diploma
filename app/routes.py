1d6b1e0900c005e922821ac066283a0295e3dc0f
edjooor <berezkinzorik@gmail.com>
2024-05-23 01:21:38 +0300

    added new page for each earthqk

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,7 @@ def get_db_connection():
                             database='earthquakes_map',
                             user='postgres',
                             password='1234')
-                            # user=os.environ['postgres'],
-                            # password=os.environ['1234'])
+                            
     return conn
 
 
@@ -17,24 +16,22 @@ def get_db_connection():
 @app.route('/')
 @app.route('/index')
 def index():
-
     conn = get_db_connection()
     cur = conn.cursor()
     cur.execute('SELECT * FROM s1;')
     earthquakes_S1 = cur.fetchall()
-    # print(earthquakes)
+    cur.close()
+    conn.close()  
+    return render_template('index.html', title='Earthquake map', earthquakes_S1=earthquakes_S1)
+
+@app.route('/earthquake/<int:earthquake_id>')
+def earthquake_detail(earthquake_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM s1 WHERE id = %s;', (earthquake_id,))
+    earthquake = cur.fetchone()
     cur.close()
     conn.close()
-   
-    # earthquakes = [
-    #     {
-    #         'date': {'date': '3/13/1990'},
-    #         'time': {'time': '05:36:01.01'},
-    #     },
-    #     {
-    #         'date': {'date': '4/12/1985'},
-    #         'time': {'time': '00:33:01.01'},
-            
-    #     }
-    # ]
-    return render_template('index.html', title='Earthquake map', earthquakes_S1=earthquakes_S1)
+    if earthquake is None:
+        return "Earthquake not found", 404
+    return render_template('earthquake_detail.html', earthquake=earthquake)
